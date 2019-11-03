29d307322cc8f62e86021c2a34f6b2ba01cf2fa9
Xeniia-droid <vksyuha1311@gmil.com>
2019-11-03 18:29:17 +0300

    add new project

---
+++ src/main/java/com/Feature1/WSFeaure1/WsFeaure1Application.java
@@ -0,0 +1,13 @@
+package com.Feature1.WSFeaure1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsFeaure1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsFeaure1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Feature1/WSFeaure1/WsFeaure1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Feature1.WSFeaure1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsFeaure1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
