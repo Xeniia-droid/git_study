a556a7176b7f32c4ff024b946318a293bdd360e7
Xeniia-droid <vksyuha1311@gmil.com>
2019-11-18 15:29:24 +0300

    add assert

--- src/test/java/com/Feature/WSFeaure/UserUnitTest.java
+++ src/test/java/com/Feature/WSFeaure/UserUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.slf4j.Logger;
+import org.springframework.util.Assert;
 
 import java.io.IOException;
 
@@ -20,6 +21,7 @@ public class UserUnitTest {
     @DisplayName("AddUserInstance")
     public void addUserTest(){
         userTest = new User(1, "Test1");
+        assert userTest != null : "Didn't create new User";
 
     }
 
@@ -27,14 +29,14 @@ public class UserUnitTest {
     @DisplayName("getUserName")
     public void getUserName(){
         userTest = new User(1, "Test1");
-        userTest.getName();
+        assert userTest.getName()!= "" : "Didn't find user by name";
     }
 
     @Test
     @DisplayName("getUserId")
     public void getUserId(){
         userTest = new User(1, "Test1");
-        userTest.getId();
+        assert userTest.getId()>0 : "Didn't find user by id";
     }
 
     @ParameterizedTest(name = "Set new User Name {0} .")
@@ -43,6 +45,7 @@ public class UserUnitTest {
     public void setUserName(String sets){
         userTest = new User(1, "Test1");
         userTest.setName(sets);
+        assert  userTest.getName().equals(sets) : "Didn't set new Name";
     }
 
 
@@ -52,8 +55,8 @@ public class UserUnitTest {
     public void setUserId(int unts){
         userTest = new User(1, "Test1");
         userTest.setId(unts);
+        assert userTest.getId()==unts : "Didn't set new id";
     }
-
 
     public void setLogger(Logger logger) {
         this.logger = logger;
